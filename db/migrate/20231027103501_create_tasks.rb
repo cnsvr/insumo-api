91b936595fe3e23e0e7ca5904e424ba91241afe7
furkancansever <furkancansever@imaginitesoft.com>
2023-10-27 14:50:14 +0300

    Enable UUID support and create tables for users and tasks

---
+++ db/migrate/20231027103501_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :tasks, id: :uuid do |t|
+      t.string :title, null: false
+      t.datetime :due_date, null: false, index: true
+      t.references :user, null: false, foreign_key: { on_delete: :cascade }, type: :uuid, index: true
+
+      t.timestamps
+    end
+  end
+end
